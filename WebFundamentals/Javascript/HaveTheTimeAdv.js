b5a19ee2361ae868dc3d50a5c91a2fc8f48cdd07
hermanj13 <hermanj13@me.com>
2017-01-29 15:44:47 -0500

    fixed bugs before continuation

--- WebFundamentals/Javascript/HaveTheTimeAdv.js
+++ WebFundamentals/Javascript/HaveTheTimeAdv.js
@@ -1,44 +1,37 @@
-var hour = 11;
-var minute = 62;
-var period = "PM";
-
-if(hour === 12 && period === "AM" && minute == 0){
+function time(hour,minute){
+if(hour === 12 && period === "AM" && minute === 0){
   console.log("It's midnight");
   return;
 }
-if(hour === 12 && period === "PM" && minute == 0){
+if(hour === 12 && period === "PM" && minute === 0){
   console.log("It's Noon");
   return;
 }
 
 if(minute > 0 && minute < 15){
-  var minDesc = "just after";
-}
-else if(minute === 15){
-  var minDesc = "quarter after";
-}
-else if(minute > 15 && minute <30){
-  var minDesc = "just after quater after";
-}
-else if(minute === 30){
-  var minDesc = "half past";
-}
-else if(minute > 30 && minute < 45){
-  var minDesc = "just after half past"
-}
-else if(minute === 45){
-  var minDesc = "quarter till"
-  hour = hour+1
-}
-else if(minute > 45 && minute < 60){
-  var minDesc = "almost"
-}
-else{
-  console.log("Invalid minutes")
+  var minute = "just after";
+} else if(minute === 15){
+  var minute = "quarter after";
+} else if(minute > 15 && minute <30){
+  var minute = "just after quater after";
+} else if(minute === 30){
+  var minute = "half past";
+} else if(minute > 30 && minute < 45){
+  var minute = "just after half past";
+} else if(minute === 45){
+  var minute = "quarter till";
+  hour = hour+1;
+} else if(minute > 45 && minute < 60){
+  var minute = "almost";
+  hour = hour+1;
+} else{
+  console.log("Invalid minutes");
   return;
 }
-console.log(minDesc)
-//
+console.log(minute);
+
+
+
 // if(period === "AM"){
 //   period = "in the morning";
 // }
@@ -49,5 +42,9 @@ console.log(minDesc)
 //   console.log("invalid period");
 //   return;
 // }
-//
-// console.log("It's", minDesc, hour, period);
+
+// console.log("It's", minute, hour, period);
+
+
+}
+time(3,63);
