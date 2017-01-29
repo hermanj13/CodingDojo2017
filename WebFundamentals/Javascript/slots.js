e488caf19e2180b824cf84af54b4f7f4e173a821
hermanj13 <hermanj13@me.com>
2017-01-29 18:40:46 -0500

    syntex

--- WebFundamentals/Javascript/slots.js
+++ WebFundamentals/Javascript/slots.js
@@ -1,18 +1,18 @@
-function slots(quarters, stopValue) {
+function slots(quarters, stopValue){
     var winner = 52;
-      while (quarters !== 0) {
+      while (quarters !== 0){
           var compare = Math.floor((Math.random()) * 100);
-          if (compare === winner) {
-              var winnings = Math.floor((Math.random() * 50) + 50)
+          if (compare === winner){
+              var winnings = Math.floor((Math.random() * 50) + 50);
               quarters = quarters - 1 + winnings;
               console.log("We Have A Winner");
-              console.log("You have " + quarters + " remaining!")
+              console.log("You have " + quarters + " remaining!");
           } else {
-              quarters = quarters - 1
-              console.log("You have " + quarters + " remaining!")
+              quarters = quarters - 1;
+              console.log("You have " + quarters + " remaining!");
           }
           if (quarters >= stopValue){
-            console.log("You have over " + stopValue + " quarters! " + (quarters - stopValue) + " more to be exact!")
+            console.log("You have over " + stopValue + " quarters! " + (quarters - stopValue) + " more to be exact!");
             break;
           }
       }
