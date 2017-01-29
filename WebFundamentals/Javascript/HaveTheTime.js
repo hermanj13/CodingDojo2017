71bf5b785159c3847dd7dc9599e3484a383754db
hermanj13 <hermanj13@me.com>
2017-01-29 14:57:09 -0500

    error consolidation

--- WebFundamentals/Javascript/HaveTheTime.js
+++ WebFundamentals/Javascript/HaveTheTime.js
@@ -1,22 +1,21 @@
 function time(hour,minute,period){
 if(minute <= 30){
   var minDesc = "just after";
-}
-else{
+} else{
   var minDesc = "almost";
   hour = hour+1;
 }
 
 if(period === "AM"){
   period = "in the morning";
-}
-else if (period === "PM"){
+} else if (period === "PM"){
   period = "in the evening";
-}
-else{
+} else{
   console.log("invalid period");
   return;
 }
 
 console.log("It's", minDesc, hour, period);
 }
+
+time(9,10,"AM");
