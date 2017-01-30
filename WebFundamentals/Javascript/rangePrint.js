0f6566a988544b3586cc31263f81cc69870fcb5c
hermanj13 <hermanj13@me.com>
2017-01-30 14:24:22 -0500

    add and alter

--- WebFundamentals/Javascript/rangePrint.js
+++ WebFundamentals/Javascript/rangePrint.js
@@ -4,4 +4,16 @@ function printRange(first,last,skip){
   }
 }
 
+// function printRange(first,last){
+//   for(var print = first; print < last; print++){
+//     console.log(print);
+//   }
+// }
+
+// function printRange(last){
+//   for(var print = 0; print < last; print++){
+//     console.log(print);
+//   }
+// }
+
 printRange(2,10,2);
