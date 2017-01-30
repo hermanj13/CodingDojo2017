8a4182d3682a349a1d3ba30cad8b38ca65f1b973
hermanj13 <hermanj13@me.com>
2017-01-30 16:42:08 -0500

    syntex error

--- WebFundamentals/Javascript/numbersOnly.js
+++ WebFundamentals/Javascript/numbersOnly.js
@@ -13,7 +13,7 @@ console.log(numbersArray([7, "apple", "blue", 3, 5]));
 // Remove numbers first method
 function removeNumbers(arr) {
     for (var i = 0; i < arr.length; i++) {
-        if (typeof arr[i] != "number") {
+        if (typeof arr[i] !== "number") {
             arr.splice(i, 1);
             i--;
         }
@@ -28,7 +28,7 @@ console.log(removeNumbers(([1, "apple", -3, "orange", 0.5, 55, 56, 57, "blue", "
 function removeNumbers2(arr) {
     var i = 0;
     while (i < arr.length) {
-        if (typeof arr[i] != "number") {
+        if (typeof arr[i] !== "number") {
             arr.splice(i, 1);
         } else {
             i++;
