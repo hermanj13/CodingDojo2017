110020dc47ffe5c054ddc96a6c0d71fa57fd4a37
hermanj13 <hermanj13@me.com>
2017-01-29 19:57:59 -0500

    numbersOnly js upload

---
+++ WebFundamentals/Javascript/numbersOnly.js
@@ -0,0 +1,12 @@
+function numbersArray(arr){
+var newArray = [];
+  for(i = 0; i < arr.length - 1; i++){
+    if(typeof arr[i] === "number"){
+      newArray.push(arr[i]);
+    }
+  }
+  return(newArray);
+}
+
+var newArray = numbersArray([7, "apple", "blue", 3,5]);
+console.log(newArray);
