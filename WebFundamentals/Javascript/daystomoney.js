1cd5b363dded609a34a8db3e22368050ee55738b
hermanj13 <hermanj13@me.com>
2017-01-30 16:01:40 -0500

    syntex

--- WebFundamentals/Javascript/daystomoney.js
+++ WebFundamentals/Javascript/daystomoney.js
@@ -1,11 +1,11 @@
 function numberDays(money){
   var total = .01;
   var days = 0;
-  while( total <= money ){
-    total = total * 2
+  while(total < money ){
+    total = total * 2;
     days++;
   }
   console.log(days);
 }
 
-numberDays(10000);
+numberDays(Infinity);
