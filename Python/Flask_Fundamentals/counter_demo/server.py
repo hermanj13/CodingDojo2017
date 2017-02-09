4ef3d5e98b01119fe0a5af8abde026180d483c68
hermanj13 <hermanj13@me.com>
2017-02-09 16:55:35 -0500

    error

--- Python/Flask_Fundamentals/counter_demo/server.py
+++ Python/Flask_Fundamentals/counter_demo/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, redirect, session
 app = Flask(__name__)
 app.secret_key = 'actAFool'
 
