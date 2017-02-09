9e2fe6bb55fdd021fd41d92e815476dd78857075
hermanj13 <hermanj13@me.com>
2017-02-09 16:51:55 -0500

    errors

--- Python/Flask_Fundamentals/counter_demo/server.py
+++ Python/Flask_Fundamentals/counter_demo/server.py
@@ -2,12 +2,13 @@ from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 app.secret_key = 'actAFool'
 
-def sumSessionCounter():
-    try:
-        session(['counter']) += 1
-    except KeyError:
-        session['counter'] = 1
-
+# def sumSessionCounter():
+#     try:
+#         session(['counter']) += 1
+#     except KeyError:
+#         session['counter'] = 1
+#
+#
 
 @app.route('/')
 def index():
