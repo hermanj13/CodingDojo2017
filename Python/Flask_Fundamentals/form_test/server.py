9d1698a051eae3a990bd76aa44604c3fd5e0d503
hermanj13 <hermanj13@me.com>
2017-02-10 14:00:17 -0500

    dojo survey added validation

--- Python/Flask_Fundamentals/form_test/server.py
+++ Python/Flask_Fundamentals/form_test/server.py
@@ -8,15 +8,25 @@ def index():
 
 @app.route('/users', methods=['POST'])
 def create_user():
+    session.pop('error', None)
+    session['comment'] = request.form['comment']
+    if len(session['comment']) > 120:
+        session['error'] = "Excuse me... You're comment is just a little to long!"
+        session.pop('comment', None)
+        return redirect('/')
     session['name'] = request.form['name']
     session['email'] = request.form['email']
     session['location'] = request.form['location']
     session['language'] = request.form['language']
-    session['comment'] = request.form['comment']
     return redirect('/results')
 
 @app.route('/results')
 def show_results():
     return render_template('results.html')
+
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
 
 app.run(debug=True)
