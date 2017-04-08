251b4a26375f9560582c6673d23d4148c0ab5c5a
Jake <hermanj13@me.com>
2017-04-08 19:23:35 -0400

    update

--- Algorithm/chapter9_recursion.js
+++ Algorithm/chapter9_recursion.js
@@ -13,3 +13,27 @@ function rFibHelp(num, count, prev, sum) {
 }
 
 console.log(rFib(23))
+
+function rSubsets(str) {
+    var arr = str.split('')
+    arr.reverse()
+    var arrFinal = []
+    var arrNew = []
+    return rSubHelp(arr, arrNew, arrFinal)
+}
+
+function rSubHelp(arr, arrNew, arrFinal) {
+    if (arr.length == 0) {
+        arrFinal.push(arrNew.join())
+        return arrFinal
+    }
+    var x = arr.pop()
+    arrNew.push(x)
+    rSubHelp(arr, arrNew, arrFinal)
+    arrNew.pop()
+    rSubHelp(arr, arrNew, arrFinal)
+    arr.push(x)
+    return arrFinal
+}
+
+console.log(rSubsets('abc'))
