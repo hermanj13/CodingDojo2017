a11199185e31bd4538f53359a0e57c64ffc81688
Jake <hermanj13@me.com>
2017-04-18 12:01:38 -0400

    finished angular fundamentals

--- Algorithm/chapter12_Sorts.js
+++ Algorithm/chapter12_Sorts.js
@@ -1,5 +1,6 @@
 function bubbleSort(arr) {
     for (var x = arr.length - 1; x >= 0; x--) {
+        var flag = true;
         for (var i = 0; i < x; i++) {
             var temp = arr[i];
             var j = i;
@@ -7,26 +8,54 @@ function bubbleSort(arr) {
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
                 j++;
+                flag = false;
             }
         }
+        if (flag === true) {
+            return arr;
+        }
     }
-    return arr
+    return arr;
 }
-blue = [13, 15, 5, 7, 12, 99, 123]
+green = [1, 2, 3, 4, 5]
+blue = [13, 14, 19, 15, 5, 7, 12, 99, 7, 123, 1]
 console.log(bubbleSort(blue))
 
 function bubbleSort2(arr) {
     for (var x = arr.length - 1; x >= 0; x--) {
+        var flag = true;
         for (var i = 0; i < x; i++) {
             var temp = arr[i];
             while (arr[i] > arr[i + 1]) {
                 arr[i] = arr[i + 1];
                 arr[i + 1] = temp;
                 i++;
+                flag = false;
             }
         }
+        if (flag === true) {
+            return arr;
+        }
     }
-    return arr
+    return arr;
 }
 
 console.log(bubbleSort2(blue))
+
+function selectionSort(arr) {
+    for (var x = 0; x < arr.length; x++) {
+        var min = arr[x];
+        for (var y = x; y < arr.length; y++) {
+            if (arr[y] <= min) {
+                min = arr[y];
+                var pos = y;
+            }
+        }
+        var temp = arr[x];
+        arr[x] = min;
+        arr[pos] = temp;
+    }
+    return arr;
+}
+
+console.log(selectionSort(blue))
